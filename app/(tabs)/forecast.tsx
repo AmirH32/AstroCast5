aaefaca6d15f4e8116c5744001406503e8de9920
jrw208 <jrw208@cam.ac.uk>
2024-05-16 15:42:56 +0100

    testing

---
+++ app/(tabs)/forecast.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DayWidget from '../daywidget.tsx'
+
+export default function Forecast() {
+    return (
+        <View>
+            <DayWidget day="Monday" />
+        </View>
+    )
+}

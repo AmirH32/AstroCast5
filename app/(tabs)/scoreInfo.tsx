c920a14cb6fddc41e56cb97f396889b6f046574b
Radhika <radhika.iyer265@gmail.com>
2024-05-16 18:06:15 +0100

    Formatting help page (colours etc)

--- app/(tabs)/scoreInfo.tsx
+++ app/(tabs)/scoreInfo.tsx
@@ -1,37 +1,28 @@
 import { Image, StyleSheet, Platform } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
+
+//Colour Hex Codes: Purple - #56244A, Blue (Back arrows etc) #1A5974, Light Shade 1 (White) - #E8E9F3, 2?? CDE8F4
+
 export default function HomeScreen() {
   return (
-    
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
+    <ThemedView style={styles.Container}>
+
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Scoring Information</ThemedText>
-        
+        <ThemedText type="title" style={{color: '#CDE8F4'}}>Scoring Information</ThemedText>
+
       </ThemedView>
-      <ThemedView style={styles.titleContainer}>
-      </ThemedView>
+
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Heuristic Goodness Score</ThemedText>
-        <ThemedText>Each day is marked as red, amber or green depending on how good of a day it is to stargaze.
-                    The individual score is based on a mathematical formula that takes temperature, cloud cover and rainfall into consideration.
+        <ThemedText type="subtitle" style={{color: 'white'}}>Heuristic Goodness Score</ThemedText>
+        <ThemedText style={{color: '#E8E9F3'}}>Each day is marked as red, amber or green depending on how good of a day it is to stargaze.
+          The individual score is based on a mathematical formula that takes temperature, cloud cover and rainfall into consideration.
         </ThemedText>
       </ThemedView>
 
-      
-    </ParallaxScrollView>
-    
+    </ThemedView>
   );
 }
 
@@ -40,16 +31,21 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     gap: 8,
+    backgroundColor: '#56244A',
+    padding: 10
   },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
+    padding: 10,
+    backgroundColor: '#56244A'
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  Container: {
+      gap: 8,
+      marginBottom: 8,
+      padding: 10,
+      backgroundColor: '#56244A'
+
+  }
+
 });

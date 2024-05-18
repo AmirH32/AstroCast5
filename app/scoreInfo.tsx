16a283ef88aa49a366c463542a68b730c3ceaf6f
Radhika <radhika.iyer265@gmail.com>
2024-05-18 23:18:57 +0100

    Sizing and correction for heuristic equation

--- app/scoreInfo.tsx
+++ app/scoreInfo.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Dimensions } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
@@ -16,20 +16,18 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle" style={{color: 'white'}}>Heuristic Goodness Score</ThemedText>
         <ThemedText style={{color: '#E8E9F3'}}>Each day is marked as red, amber or green depending on how good of a day it is to stargaze.
-          The individual score is based on a mathematical formula that takes temperature, cloud cover and rainfall into consideration.
+          An individual score is also provided, based on a mathematical formula that takes temperature, cloud cover and rainfall into consideration.
         </ThemedText>
-      </ThemedView>
 
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText style={{color: '#E8E9F3'}}>The exact formula used is: </ThemedText>
-        <Image source={require('@/assets/images/equation4.png')} style={{ alignSelf: 'center' }} />
-        <Image source={require('@/assets/images/equation5.png')} style={{ alignSelf: 'center' }} />
+        <ThemedText style={{color: '#E8E9F3'}}>The exact formula used is as follows: </ThemedText>
+        <Image source={require('@/assets/images/equation1.png')} resizeMode = 'contain' style={{ width: "100%", alignSelf: 'center' }} />
+        <Image source={require('@/assets/images/equation2.png')} resizeMode = 'contain' style={{ width: "75%", alignSelf: 'center' }} />
       </ThemedView>
 
     </ThemedView>
   );
 }
-
+const {height, width} = Dimensions.get('window');
 const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: 'row',
@@ -39,24 +37,23 @@ const styles = StyleSheet.create({
     padding: 10
   },
   stepContainer: {
+    flex:1,
     gap: 8,
     marginBottom: 8,
     padding: 10,
     backgroundColor: '#56244A'
   },
   Container: {
+      flex: 1,
       gap: 8,
       marginBottom: 8,
       padding: 10,
       backgroundColor: '#56244A'
-
   },
 
-  image: {
-    flex: 1,
-    width: '100%',
-    height: '100%',
-    resizeMode: 'contain',
+  Image: {
+    alignSelf: 'center',
+    flex:1
 }
 
 });
